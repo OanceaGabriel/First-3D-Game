996872fdef4ba400c9c6ef8ca501543a8911648e
OanceaGabriel <gabriel.oancea400@gmail.com>
2023-08-14 15:19:34 +0300

    Added Options Menu and more functionality to main menu

--- Running cube/Assets/Scripts/Main_Menu.cs
+++ Running cube/Assets/Scripts/Main_Menu.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Start_Game : MonoBehaviour
+public class Main_Menu : MonoBehaviour
 {
    public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("QUIT GAME!");
+        Application.Quit();
     }
 }
